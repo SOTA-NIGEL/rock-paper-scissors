f52cfbd0164e1b50726a5c97826b22a6c611aa07
Nigel Panashe Sota <sirspotsotar@gmail.com>
2025-04-04 16:58:22 +0200

    updated

--- rock, paper, scissors.py
+++ rock, paper, scissors.py
@@ -1,36 +1,39 @@
+# Rock, Paper, Scissors Game
+# This is a simple implementation of the Rock, Paper, Scissors game.
 import random
 
 def game():
-    while True:
-        user_choice = input("Enter a choice (rock, paper, scissors): ").lower()
-        while user_choice not in ["rock", "paper", "scissors"]:
-            user_choice = input("Invalid input. Please enter rock, paper, or scissors: ").lower()
+    choices = ['rock', 'paper', 'scissors']
+    computer = random.choice(choices)
 
-        possible_choices = ["rock", "paper", "scissors"]
-        computer_choice = random.choice(possible_choices)
-        print(f"\nUser chose {user_choice}, computer chose {computer_choice}.\n")
+    user = input('Choose between rock, paper or scissors? ').lower().strip()
+    while user not in choices:
+        user = input('Invalid input. Please enter rock, paper, or scissors: ').lower().strip()
 
-        if user_choice == computer_choice:
-            print(f"Both players selected {user_choice}. It's a tie!")
-        elif user_choice == "rock":
-            if computer_choice == "scissors":
-                print("Rock smashes scissors! You win!")
-            else:
-                print("Paper covers rock! You lost.")
-        elif user_choice == "paper":
-            if computer_choice == "rock":
-                print("Paper covers rock! You win!")
-            else:
-                print("Scissors cuts paper! You lost.")
-        elif user_choice == "scissors":
-            if computer_choice == "paper":
-                print("Scissors cuts paper! You win!")
-            else:
-                print("Rock smashes scissors! You lost.")
+    print(f'\nComputer chose {computer}, you chose {user}.\n')
 
-        play_again = input("Play again? (yes/no): ").lower()
-        while play_again not in ["yes", "no"]:
-            play_again = input("Invalid input. Please enter yes or no: ").lower()
-        if play_again != "yes":
-            break
+    if user == computer:
+        print('It\'s a tie!')
+    elif user == 'rock':
+        if computer == 'scissors':
+            print('Rock smashes scissors! You win!')
+        else:
+            print('Paper covers rock! You lose.')
+    elif user == 'paper':
+        if computer == 'rock':
+            print('Paper covers rock! You win!')
+        else:
+            print('Scissors cuts paper! You lose.')
+    elif user == 'scissors':
+        if computer == 'paper':
+            print('Scissors cuts paper! You win!')
+        else:
+            print('Rock smashes scissors! You lose.')
 
+if __name__ == '__main__':
+    game()
+    play_again = input('Do you want to play again? (yes/no): ').lower().split()
+    if play_again == 'yes':  
+        game()
+    else :
+        print('Thanks for playing!')
